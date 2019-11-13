3f6aeab9dd96033c264f53e4e371058efb989ef2
Steven <rliu3365@baysidehighschool.org>
2019-11-13 13:46:27 -0500

    Add files via upload

---
+++ try 1.py
@@ -0,0 +1,84 @@
+#project
+import turtle
+
+bob=turtle.Turtle()
+turtle.speed(900)
+
+for times in range(360):
+    bob.circle(100)
+    bob.right(10)
+jump(300,0)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(-300,0)
+
+bob.speed(0)
+jump(200
+for times in range(360):
+    bob.circle(100)
+    jump(0,0)
+    bob.right(1)
+jump(300,0)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(-300,0)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(0,300)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(0,-300)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(220,220)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(-220,-220)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(-220,220)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+jump(220,-220)
+for times in range(40):
+    bob.color("blue")
+    bob.circle(50)
+    bob.right(10)
+bob.color("black")
+bob.right(40)
+jump(0,-300)
+bob.circle(300)
+jump(0,0)
+
+for times in range(50):
+    bob.forward(300)
+    bob.right(120)
+    bob.forward(300)
+    bob.right(120)
+    bob.forward(300)
+    bob.right(355)
+
+for times in range(70):
+    bob.forward(270)
+    bob.right(120)
+    bob.forward(270)
+    bob.right(120)
+    bob.forward(270)
+    bob.right(355)
+
